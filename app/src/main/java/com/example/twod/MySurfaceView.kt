fd40a2f59bd52813f5a352468641b75098377964
Student <39177937wasde@gmail.com>
2022-04-26 11:25:16 +0800

    2Dgame in class version 1

---
+++ app/src/main/java/com/example/twod/MySurfaceView.kt
@@ -0,0 +1,23 @@
+package com.example.twod
+
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import javax.security.auth.callback.Callback
+
+class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),SurfaceHolder.Callback {
+    override fun surfaceCreated(p0: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+
+    }
+
+}
